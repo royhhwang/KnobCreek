e850d257aa085170f098cdd3870fe4cf01361a67
Roy Hwang <royhhwang@gmail.com>
2018-10-27 19:26:51 -0700

    desktop responsiveness hero added

--- assets/javascript/modules/Feature.js
+++ assets/javascript/modules/Feature.js
@@ -1,21 +1,21 @@
-import data from '../data/DataSort';
-var landing = data.shift();
+// import data from '../data/DataSort';
+// var landing = data.shift();
 
-const featured = `
-<section class="bg-dark-tan" key="${landing.key}">
-    <aside>
-        <img src="${landing.image}" alt="${landing.alt}" />
-        <div class="__featured centered-text bg-light-tan">
-            <img src="${landing.icon}" alt="Article Icons" class="__medium-icon" />
-            <h5 class="font-size-xs">${landing.display}</h5>
-            <h2>${landing.title}</h2>
-            <p>${landing.para}</p>
-            <a href="${landing.a}">${landing.link}</a>
-        </div>
-    </aside>
-</section>
-`;
+// const featured = `
+// <section class="bg-dark-tan" key="${landing.key}">
+//     <aside>
+//         <img src="${landing.image}" alt="${landing.alt}" />
+//         <div class="__featured centered-text bg-light-tan">
+//             <img src="${landing.icon}" alt="Article Icons" class="__medium-icon" />
+//             <h5 class="font-size-xs">${landing.display}</h5>
+//             <h2>${landing.title}</h2>
+//             <p>${landing.para}</p>
+//             <a href="${landing.a}">${landing.link}</a>
+//         </div>
+//     </aside>
+// </section>
+// `;
 
-document.getElementById('feature').innerHTML = featured;
+// document.getElementById('feature').innerHTML = featured;
 
-export default featured;
+// export default featured;
