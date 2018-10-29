4b891eaa35c5ece5db1252db8679c1316c6d9ab8
Roy Hwang <royhhwang@gmail.com>
2018-10-29 06:22:17 -0700

    templates, responsiveness, images optimized

--- assets/javascript/modules/Feature.js
+++ assets/javascript/modules/Feature.js
@@ -2,14 +2,14 @@ import data from '../data/DataSort';
 var landing = data.shift();
 
 const featured = `
-<div class="hero__section bg-dark-tan" key="${landing.key}">
-    <img src="${landing.image}" alt="${landing.alt}" class="hero__image"/>
-    <div class="hero__content centered-text">
-        <img src="${landing.icon}" alt="Article Icons" class="hero__icon icon-med" />
-        <h5 class="hero__display font-size-xs">${landing.display}</h5>
-        <h2 class="hero__title">${landing.title}</h2>
+<div class="card" key="${landing.key}">
+    <div class="card__image" style="background-image: url('${landing.image}')"></div>
+    <div class="card__content">
+        <img class="icon" src="${landing.icon}" alt="Article Icons" />
+        <p class="emphasis-text">${landing.display}</p>
+        <h2 class="subheading">${landing.title}</h2>
         <p>${landing.para}</p>
-        <a href="${landing.a}" class="hero__link">${landing.link}</a>
+        <a href="${landing.a}" class="red-link">${landing.link}</a>
     </div>
 </div>
 `;
--- assets/javascript/modules/Templates.js
+++ assets/javascript/modules/Templates.js
@@ -2,16 +2,19 @@ import data from '../data/DataSort';
 
 const articles = `
 ${data.slice(1).map(bourbon => `
-<li class='article__card' key="${bourbon.key}">
-    <img src="${bourbon.image}" alt="${bourbon.alt}" class="article__image" />
-    <div class='article__content'>
-        <img src="${bourbon.icon}" alt="Article Icons" class="icon-xs" />
-        <div class="article__circle__base"><div class="article__circle">&nbsp;</div></div>
-        <h5 class="font-size-xs article__display">${bourbon.display}</h5>
-        <h3 class="article__title">${bourbon.title}</h3>
-        <a href='${bourbon.a}' class="article__redirect">${bourbon.link}</a>
-    </div>
-</li>
+<li class="article__list__item" key="${bourbon.key}>
+            <div class="card">
+              <div class="card__image" style="background-image: url('${bourbon.image}')"></div>
+              <div class="card__content">
+                <div class="circle-icon">
+                  <img src="${bourbon.icon}" alt="Article Icons" class="icon" />
+                </div>
+                <p class="emphasis-text">${bourbon.display}</p>
+                <h2 class="subheading-2">${bourbon.title}</h2>
+                <a href="${bourbon.a}" class="red-link">${bourbon.link}</a>
+              </div>
+            </div>
+          </li>
 `).join('')}
 `;
 
